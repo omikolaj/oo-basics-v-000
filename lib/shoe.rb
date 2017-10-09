1c38ebdec650e71ea3958ab039b9ed6467c7a2da
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-09 09:25:49 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,9 @@
 # Make your shoe class here!
 class Shoe
-attr_accessor :brand, :color, :size, :material, :condition,
-
+attr_accessor :color, :size, :material, :condition, :brand
+attr_reader :brand
 def initialize(brand)
   @brand
 end
-
-
 
 end
